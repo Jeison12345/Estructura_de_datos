e49d1aeea3d4077797a512eeaad40d0a3a3e0478
Jeison Rico <jeisonrico13@gmail.com>
2023-05-03 11:12:11 -0500

    Se agrega encontrar numero en un vector

--- Programacion_imperativa.c
+++ Programacion_imperativa.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int suma_elementos_vector(int vector[], int longitud){
 /**
@@ -15,9 +16,30 @@ int suma_elementos_vector(int vector[], int longitud){
     return suma;
 }
 
+bool encontrar_numero(int vector[], int longitud, int numero){
+/**
+ * @brief Busca un numero en un vector
+ * @param vector[] Vector de enteros
+ * @param longitud Longitud del vector
+ * @param numero Numero a buscar
+ * @return bool True si el numero esta en el vector, False si no esta
+ */
+
+    for(int i = 0; i < longitud; i++){
+        if(vector[i] == numero){
+            return true;
+        }
+    }
+    return false;
+}
+
 int main(){
     int vector[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int longitud = sizeof(vector)/sizeof(vector[0]);
+    int numero;
     printf("La suma del vector es: %i\n", suma_elementos_vector(vector, longitud));
+    printf("Introduce un numero: ");
+    scanf("%i", &numero);
+    printf("El numero %i %s esta en el vector\n", numero, encontrar_numero(vector, longitud, numero) ? "si" : "no");
     return 0;
 }
